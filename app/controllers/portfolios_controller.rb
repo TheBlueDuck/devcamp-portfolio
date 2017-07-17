4068ff0aaa19da4163eb7c92fb71d5ec296291c1
slcwestdevcamp <slcwest@devcamp.com>
2017-07-17 15:11:15 +0000

    Implemented persistent position updates for drag and drop interface

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,10 +1,18 @@
 class PortfoliosController < ApplicationController
     before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
-    layout "portfolio"
+    layout 'portfolio'
     access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
     
     def index
         @portfolio_items = Portfolio.by_position
+    end
+    
+    def sort
+        params[:order].each do |key, value|
+            Portfolio.find(value[:id]).update(position: value[:position])
+        end
+        
+        render nothing: true
     end
     
     def angular
@@ -18,29 +26,27 @@ class PortfoliosController < ApplicationController
     
     def create
         @portfolio_item = Portfolio.new(portfolio_params)
-
+        
         respond_to do |format|
-        if @portfolio_item.save
-            format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live. ' }
-        else
-            format.html { render :new }
+            if @portfolio_item.save
+                format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
+            else
+                format.html { render :new }
+            end
         end
-      end
     end
-     
+    
     def edit
     end
     
     def update
         respond_to do |format|
-        if @portfolio_item.update(portfolio_params)
-            format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
-            format.json { render :show, status: :ok, location: @blog }
-        else
-            format.html { render :edit }
-            format.json { render json: @blog.errors, status: :unprocessable_entity }
+            if @portfolio_item.update(portfolio_params)
+                format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
+            else
+                format.html { render :edit }
+            end
         end
-      end
     end
     
     def show
@@ -50,8 +56,8 @@ class PortfoliosController < ApplicationController
         # Destroy/delete the record
         @portfolio_item.destroy
         
-            #Redirect
-            respond_to do |format|
+        # Redirect
+        respond_to do |format|
             format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
         end
     end
